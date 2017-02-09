710c624040f7611d0c81225776001de17ab90a70
Francisco Calaça Xavier <chicocx@gmail.com>
2017-02-09 10:24:10 -0200

    First commit

---
+++ Assets/Player.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour
+{
+
+    public int moveX = 0;
+
+    public int moveY = 0;
+
+    public float moveSpeed = 0.2f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(moveX * moveSpeed, moveY * moveSpeed, 0);
+
+        if (isLocalPlayer)
+        {
+            moveX = 0;
+            moveY = 0;
+
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                moveX -= 1;
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                moveX += 1;
+            }
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                moveY += 1;
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                moveY -= 1;
+            }
+        }
+    }
+
+    public void FixedUpdate()
+    {
+        transform.Translate(moveX * moveSpeed, moveY * moveSpeed, 0);
+    }
+
+}
